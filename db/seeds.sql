ce000cc8d226c1966417ad9852d8080c8f29be1b
Jody Eggleston <jmeggles@gmail.com>
2020-07-18 13:22:49 -0400

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO burgers (burger_name) 
+VALUES ("Plain Ol' Hamburger");
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Cheesy Burger");
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Sweet N Spicy Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Not Your Father's Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("The Chick Burger", true);
+
+INSERT INTO burgers (burger_name) 
+VALUES ("You're Such a Turkey! Burger");
