555774b395693070e2af1d1287d3591c1846e4c6
Jody Eggleston <jmeggles@gmail.com>
2020-07-18 13:20:32 -0400

    updated config files

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,15 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
+// Set up MySQL connection.
 var mysql = require("mysql");
-
-var app = express();
-
-var PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "rootroot",
-  database: "burgerDB"
+  database: "burgers_db"
 });
 
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -31,4 +18,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
