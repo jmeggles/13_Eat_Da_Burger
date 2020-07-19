2aef8d074b423afcdfb2cc869c595ec1fa58f414
Jody Eggleston <jmeggles@gmail.com>
2020-07-19 19:52:26 -0400

    adding connection info for heroku through sql pro

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,31 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "o677vxfi8ok6exrd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "qgyw2xr2pz89gvg1",
-  password: "qdx80bkt6seoredw",
-  database: "di3ccqo3mmar47t6"
-});
+// make connection with JAWS db
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}  else {
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+  });
+}
+
+// removing this section to redirect through sql pro for heroku
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// make connection with database
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
